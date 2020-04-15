d8a0c2accfabd2f21c8b4a32cd02d98f363800f9
tschux <mayr.juergen@gmail.com>
2020-04-16 01:12:43 +0200

    added province positive map

--- testp.R
+++ testp.R
@@ -9,7 +9,7 @@ library(ggplot2)
 library(gganimate)
 library(viridis)
 library(leaflet.minicharts)
-titletext <- paste0("Data Source: Provinz  Bozen <br />  Date : ", Sys.Date(), "<br /> Created with: R by J. Mayr")
+titletext <- paste0("Data Source:COVID19 github repository Umberto Rosini.  Provinz  Bozen <br />  Date : ", Sys.Date(), "<br /> Created with: R by J. Mayr")
 pal <- colorNumeric("viridis", NULL)
 temp = tempfile(fileext = ".xlsx")
 tilesURL <- rgdal::readOGR("https://raw.githubusercontent.com/openpolis/geojson-italy/master/geojson/limits_IT_provinces.geojson")
@@ -25,6 +25,10 @@ pcmdpcfr <- read.csv(curl("https://raw.githubusercontent.com/pcm-dpc/COVID-19/ma
 pcmdpcr <- filter(pcmdpcfr,pcmdpcfr$lat > 0 & pcmdpcfr$long >0)
 pcmdpcr$data <- as.Date(as.factor(gsub("T.*","",pcmdpcr$data)))
 ev <- ev %>%  rename(sigla_provincia = sigla)
+testorder <- data.frame()
+testorder <- as.data.frame(as.character(tilesURL@data[["prov_acr"]]))
+names(testorder) <- "sigla_provincia"
+ev <- full_join(testorder,ev,  by = "sigla_provincia")
 # pcmdpcr<- pcmdpcr%>% select(reg_istat_code, totale_positivi,isolamento_domiciliare,variazione_totale_positivi,dimessi_guariti,deceduti)%>% group_by(reg_istat_code) %>% summarise_all(sum)
 pcmdpcr <-full_join(ev, pcmdpcr,  by = "sigla_provincia")
 # pcmdpcr <- pcmdpcr[order(pcmdpcr$reg_istat_code),]
@@ -32,6 +36,6 @@ leaflet(tilesURL) %>%
   addProviderTiles(providers$Esri.WorldImagery, group = "World Imagery") %>%
   addProviderTiles(providers$CartoDB.Positron)%>%
   addLayersControl(baseGroups = c("Carto DB Positron", "World Imagery")) %>%
-  #  addTiles(urlTemplate = 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png') %>%
-  addPolygons(stroke = F, smoothFactor = 0.1, fillOpacity = .7, fillColor = ~pal(pcmdpcr$totale_casi/pcmdpcr$residenti*100),labelOptions = labelOptions(textsize = "15px",opacity = 0.7), label = lapply(paste0("<b>",pcmdpcr$provincia,"</b> <br />  in Positive in %: ", formatC(pcmdpcr$totale_casi/pcmdpcr$residenti*100, big.mark = ","),"<br />  Fälle: ", formatC(pcmdpcr$totale_casi, big.mark = ","))), htmltools::HTML) %>%
+  addPolygons(stroke = F,fillOpacity = .7,fillColor = ~pal(pcmdpcr$totale_casi/pcmdpcr$residenti*100),labelOptions = labelOptions(textsize = "15px",opacity = 0.7),label = lapply(paste0("<b>",pcmdpcr$provincia,"</b> <br />  in Positive in %: ",formatC( pcmdpcr$totale_casi/pcmdpcr$residenti*100),"<br />  Fälle: ",pcmdpcr$totale_casi ), htmltools::HTML))%>%
+ # addPolygons(stroke = F, smoothFactor = 0.1, fillOpacity = .7, fillColor = ~pal(pcmdpcr$totale_casi/pcmdpcr$residenti*100),labelOptions = labelOptions(textsize = "15px",opacity = 0.7), label = lapply(paste0("<b>",pcmdpcr$provincia,"</b> <br />  in Positive in %: ", formatC(pcmdpcr$totale_casi/pcmdpcr$residenti*100, big.mark = ","),"<br />  Fälle: ", formatC(pcmdpcr$totale_casi, big.mark = ","))), htmltools::HTML) %>%
   addLegend(pal = pal,title = "Positive in % zur EZ",labFormat = labelFormat(),na.label = "Keine Angabe",position = "bottomright", values = ~pcmdpcr$totale_casi/pcmdpcr$residenti*100, opacity = .7)%>% addControl(titletext, position = "topright", className="map-title")
